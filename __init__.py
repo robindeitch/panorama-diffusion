4aca96f84c0e4d7819a547ee81cdac5f252e65df
Robin Deitch <robindeitch@gmail.com>
2024-10-10 13:20:12 +0100

    Added custom operator

--- __init__.py
+++ __init__.py
@@ -7,9 +7,19 @@ bl_info = {
 
 import bpy
 
+class RunDiffusionPanoramaOp(bpy.types.Operator):
+    '''Test tooltip'''
+    bl_idname = "panorama_diffusion.run"
+    bl_label = "Test instructions"
+
+    def execute(self, context):
+        print("HELLO")
+        return {"FINISHED"}
+
+
 class PanoramaDiffusionPanel(bpy.types.Panel):
-    bl_idname = "OBJECT_PT_select"
-    bl_label = "Select"
+    bl_idname = "VIEW3D_PT_panorama_diffusion"
+    bl_label = "Panorama Diffusion"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'Pano'
@@ -21,16 +31,14 @@ class PanoramaDiffusionPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
 
-        box = layout.box()
-        box.label(text="Selection Tools")
-        box.operator("object.select_all").action = 'TOGGLE'
-        row = box.row()
-        row.operator("object.select_all").action = 'INVERT'
-        row.operator("object.select_random")
+        row = layout.row()
+        row.operator("panorama_diffusion.run", text="Run pano diff")
 
 
 def register():
+    bpy.utils.register_class(RunDiffusionPanoramaOp)
     bpy.utils.register_class(PanoramaDiffusionPanel)
 
 def unregister():
     bpy.utils.unregister_class(PanoramaDiffusionPanel)
+    bpy.utils.unregister_class(RunDiffusionPanoramaOp)
